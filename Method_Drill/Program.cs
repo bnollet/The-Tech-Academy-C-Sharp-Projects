f0b5f9ac3c335f2551e1345d6b6017f7063ed67c
bnollet <bnollet7@gmail.com>
2019-01-07 19:46:24 -0700

    Method Drill

---
+++ Method_Drill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Drill
+{
+    class Program : SimpleMath
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please pick a number ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            int addingResult = SimpleMath.AddTwoNumbers(userNumber);
+            int subtractResult = SimpleMath.SubtractTwoNumbers(userNumber);
+            int productResult = SimpleMath.MultiplyTwoNumbers(userNumber);
+
+            Console.WriteLine("the adding result is " + addingResult);
+            //Console.ReadLine();
+
+            Console.WriteLine("the subtraction result is " + subtractResult);
+            //Console.ReadLine();
+
+            Console.WriteLine("the Multiplication result is " + productResult);
+            Console.ReadLine();
+
+      
+        }
+    }
+}
---
+++ Method_Drill/SimpleMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Drill
+{
+    public class SimpleMath
+    {
+        public static int AddTwoNumbers(int userNumber)
+        {
+            return userNumber + 2;
+        }
+
+        public static int SubtractTwoNumbers(int userNumber)
+        {
+            return userNumber - 1;
+        }
+
+        public static int MultiplyTwoNumbers(int userNumber)
+        {
+            return userNumber * 2;
+        }
+    }
+}
