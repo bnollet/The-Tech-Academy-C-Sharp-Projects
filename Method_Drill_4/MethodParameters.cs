d99c27d522eeea6dabe0ad4123a05e03a4e10c80
bnollet <bnollet7@gmail.com>
2019-01-09 20:06:16 -0700

    Method_Drill_4

---
+++ Method_Drill_4/MethodParameters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Drill_4
+{
+   
+        public class Addition
+        {
+            public int AddTwoValues(int x, int y)
+            {
+                Console.WriteLine("the number is " + x);
+                return x + y;
+            }
+
+            public int AddTwoValues(double x, double z)
+            {
+            return (int)(x * z);
+            }
+
+            public string AddTwoValues(string x, string z)
+        {
+            return (x + z);
+                //(Convert.ToInt32(x)) + (Convert.ToInt32(z));
+        }
+        }
+}
---
+++ Method_Drill_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Drill_4
+{
+    class Program
+    {
+        public class MethodParameters
+        {
+            static void Main()
+            {
+                Addition a = new Addition();
+                int x = a.AddTwoValues(12, 13);
+                int y = a.AddTwoValues(12, 13.05);
+                string z = a.AddTwoValues("two", "four");
+
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+                Console.WriteLine(z);
+                Console.ReadLine();
+            }
+        }
+    }
+}
