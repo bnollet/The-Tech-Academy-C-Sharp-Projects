8c39df4b1cedbe602a4f99d67aceec70bb4a0df4
bnollet <bnollet7@gmail.com>
2019-01-09 19:12:38 -0700

    Method_drill_3

---
+++ Method_3/MethodParameters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_3
+{
+    public class Addition
+    {
+        public int AddTwoValues(int x, int y)
+        {
+            Console.WriteLine("the number is " + x);
+            return x + y;
+        }
+
+        public int AddThreeValues(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+    }
+
+}
---
+++ Method_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Addition a = new Addition();
+                int x = a.AddTwoValues(12, 13);
+
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
