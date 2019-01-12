dcd4c5e263eb969984f630a1ae558f4d1dca958d
bnollet <bnollet7@gmail.com>
2019-01-12 12:57:01 -0700

    Method_Drill_5
    step 115

---
+++ Method_Drill_5/MethodParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Drill_5
+{
+    class Addition
+    {
+        public int AddTwoNumbers(int x, int y=5)
+        {
+            return x + y;
+        }
+        //public int AddTwoNumbers(int x, int z)
+        //{
+        //    return x + z;
+        //}
+    }
+}
---
+++ Method_Drill_5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Drill_5
+{
+    class Program
+    {
+        public class MethodParameters
+        {
+            static void Main()
+            {
+                Addition a = new Addition();
+                Console.WriteLine("Pick a number ");
+                int userInput1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("If you like to, please pick another number ");
+                string userInput2 = Console.ReadLine();
+
+
+                if (Int32.TryParse(userInput2, out int result))
+                         {
+                             int x = a.AddTwoNumbers(userInput1, result);
+                             Console.WriteLine(x);
+                        }
+                       else 
+                         {
+                            int y = a.AddTwoNumbers(userInput1);
+                            Console.WriteLine(y);
+                        }
+                Console.ReadLine();
+             }
+
+                //        }
+                
+
+
+
+                //Console.WriteLine(x);
+                //Console.ReadLine();
+
+
+
+
+                //bool success = Int32.TryParse(x,y)
+                //if success = true
+
+            }
+        }
+       
+    }
+
