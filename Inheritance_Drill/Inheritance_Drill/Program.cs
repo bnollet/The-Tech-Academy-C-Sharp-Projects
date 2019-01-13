b34167a9d9fa095ab9a639c4286b23dbb8db25ac
bnollet <bnollet7@gmail.com>
2019-01-13 12:30:34 -0700

    inheritance_Drill
    Step 120 tech Academy

---
+++ Inheritance_Drill/Inheritance_Drill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee a = new Employee();
+            a.FirstName = "Sample ";
+            a.LastName = "Student";
+            a.SayName();
+            Console.ReadLine();
+            
+        }
+    }
+}
