a41fdd0ba756151425eae38ae2189b7a781207ac
bnollet <bnollet7@gmail.com>
2019-01-13 11:12:22 -0700

    Method_Drill_6

---
+++ Method_6/Method_6/NumberManipulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_6
+{
+    public class NumberManipulator
+    {
+        public int DivideTwoNumbers(int x)
+        {
+            return x / 2;
+        }
+
+       public int outPutNumbers(out int x, out int y)
+        {
+            Console.WriteLine("Enter the first value: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second value: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            return x + y;
+        }
+    }
+}
---
+++ Method_6/Method_6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CalculatorApplication
+    {
+        public class NumberManipulator
+        {
+
+        static void Main()
+        {
+            NumberManipulator a = new NumberManipulator();
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int x = a.DivideTwoNumbers(userInput);
+
+            Console.WriteLine("the result is " + x);
+            Console.ReadLine();
+        }
+       
+
+
+        //public void getValues(out int x, out int y)
+        //{
+        //    Console.WriteLine("Enter the first value: ");
+        //    x = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.WriteLine("Enter the second value: ");
+        //    y = Convert.ToInt32(Console.ReadLine());
+        //}
+        //static void Main(string[] args)
+        //    {
+        //        NumberManipulator n = new NumberManipulator();
+
+        //        /* local variable definition */
+        //        int a, b;
+
+        //        /* calling a function to get the values */
+        //        n.getValues(out a, out b);
+
+        //        Console.WriteLine("After method call, value of a : {0}", a);
+        //        Console.WriteLine("After method call, value of b : {0}", b);
+        //        Console.ReadLine();
+        //    }
+        }
+    }
+
