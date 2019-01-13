572af789aa82dbafe4d8f1e7b4854e5044b90d60
bnollet <bnollet7@gmail.com>
2019-01-13 16:18:49 -0700

    Interface_Drill
    Interface Drill

---
+++ Interface_Drill/Abstarct _Drill/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstarct__Drill
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Hello " + firstName + lastName);
+            Console.ReadLine();
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Hello there! ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Interface_Drill/Abstarct _Drill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstarct__Drill
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Employee a = new Employee();
+            a.firstName = ("Sample ");
+            a.lastName = ("Student ");
+            a.SayName();
+
+            IQuittable b = new Employee();
+            b.Quit();
+
+        }
+    }
+}
