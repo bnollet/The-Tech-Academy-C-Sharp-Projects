ea08949d101533477e81e9739a94d8d1af9eb87a
bnollet <bnollet7@gmail.com>
2019-01-15 20:09:50 -0700

    Generic_Drill

---
+++ Overload_operators2/Abstarct _Drill/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstarct__Drill
+{
+    public class Employee<T> //: Person, IQuittable
+    {
+        //public override void SayName()
+        //{
+        //    Console.WriteLine("Hello " + firstName + lastName);
+        //    Console.ReadLine();
+        //}
+        //public void Quit()
+        //{
+        //    Console.WriteLine("Hello there! ");
+        //    Console.ReadLine();
+        //}
+
+        public List<T> things { get; set; }
+
+
+
+        //public int ID { get; set; }
+
+        //public static bool operator==(Employee one, Employee two)
+        //{
+        //    return (one == two);
+        //}
+        //public static bool operator !=(Employee one, Employee two)
+        //{
+        //    return !(one == two);
+
+        //}
+
+
+    }
+}
---
+++ Overload_operators2/Abstarct _Drill/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstarct__Drill
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            //Employee a = new Employee();
+            //a.firstName = ("Sample ");
+            //a.lastName = ("Student ");
+            //a.SayName();
+
+            //IQuittable b = new Employee();
+            //b.Quit();
+
+            //Employee one = new Employee();
+            //one.ID = 1;
+
+
+            //Employee two = new Employee();
+            //two.ID = 2;
+
+            Employee<string> Things = new Employee<string>();
+            Employee<int> Things2 = new Employee<int>();
+            Things.things = new List<string> { "car", "bike", "bottle" };
+            Things2.things = new List<int> { 1, 2, 3 };
+
+            foreach ( string things in Things.things)
+            { 
+                Console.WriteLine(things);
+            }
+
+            foreach (int things in Things2.things)
+            {
+                Console.WriteLine(things);
+            }
+            Console.ReadLine();
+            }
+
+
+        }
+    }
+
