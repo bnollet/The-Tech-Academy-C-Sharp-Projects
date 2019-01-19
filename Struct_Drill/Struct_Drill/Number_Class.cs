3bb4e29133f4cfe1145a768497215dc402d63155
bnollet <bnollet7@gmail.com>
2019-01-19 13:52:35 -0700

    Struct_Drill

---
+++ Struct_Drill/Struct_Drill/Number_Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Drill
+{
+    class Number_Class
+    {
+    }
+}
---
+++ Struct_Drill/Struct_Drill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number1 = new Number();
+            number1.Amount = 10;
+
+            Console.WriteLine(number1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
