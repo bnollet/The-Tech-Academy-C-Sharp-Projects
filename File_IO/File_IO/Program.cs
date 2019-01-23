7366b21c7faa5c3e9166348b58c437282db6b0de
bnollet <bnollet7@gmail.com>
2019-01-22 18:53:52 -0700

    File_IO_Drill

---
+++ File_IO/File_IO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string input = (Console.ReadLine());
+            
+
+            File.WriteAllText(@"C:\Users\Brenden\logs\logs.txt", input);
+            Console.WriteLine(input);
+            Console.ReadLine();
+
+        }
+    }
+}
