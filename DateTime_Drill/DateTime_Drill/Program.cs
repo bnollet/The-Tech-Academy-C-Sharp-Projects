00a02d43f6929fa4b0db249def048e15a4206c21
bnollet <bnollet7@gmail.com>
2019-01-22 19:25:05 -0700

    Date_Time_Drill

---
+++ DateTime_Drill/DateTime_Drill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+
+            Console.WriteLine("Please type a number ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            DateTime xHourslater = currentTime.AddHours(x);
+            Console.WriteLine(xHourslater);
+
+            Console.ReadLine();
+
+            //private DateTime AddHours(double x);
+           // public DateTime AddHours(double value);
+
+            
+        }
+    }
+}
