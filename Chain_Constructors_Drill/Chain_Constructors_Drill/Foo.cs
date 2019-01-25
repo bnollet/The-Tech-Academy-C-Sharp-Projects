d9e69bbe263d73daa32c51414006aa29be4ae68b
bnollet <bnollet7@gmail.com>
2019-01-24 20:00:46 -0700

    Chain_Constructor_Drill

---
+++ Chain_Constructors_Drill/Chain_Constructors_Drill/Foo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_Constructors_Drill
+{
+   public class Foo
+    {
+        private int id;
+        private string name;
+        public Foo() : this(0, "")
+        {
+        }
+        public Foo(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public Foo(int id) : this(id, "")
+        {
+        }
+        public Foo(string name) : this(0, name)
+        {
+        }
+    }
+}
---
+++ Chain_Constructors_Drill/Chain_Constructors_Drill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_Constructors_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo a = new Foo(), b = new Foo(456, "def"), c = new Foo(123), d = new Foo("abc");
+
+            var name = ("redman");
+            const string realName = "Reggie";
+
+            Console.WriteLine("He goes by {0} but his real name is {1} ", name, realName);
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
